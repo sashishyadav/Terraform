03d57cc9ae65264a2d09ef0f7af8414cf9ba18e5
sashishyadav <112395162+sashishyadav@users.noreply.github.com>
2025-02-26 18:50:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "path/to/terraform.tfstate"
+    bucket         = "tfstate-lambda-ram"
+    key            = "/statefiles"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
